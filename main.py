bbb6e1d8eee1fd408aab2f47c12cf7a62a95b185
Eloiza <eloiza.rossetto@gmail.com>
2021-02-09 17:20:01 -0300

    Encapsulamento de funções implementado

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import pandas as pd
 import numpy as np 
 
-
-def main():
-
-	df = pd.read_csv("Data/symptoms.csv")
-	print(df["question"])
-
-	questions = df["question"].to_list()
-
-	answers = []
-	i = 0
-	while (i < len(questions)):
-		print(str(i +1) + '. '+ questions[i])
-		a = input().lower()
-
-		if(a == "sim" or a == "s" or a == "1"):
-			answers.append(1)
-			i += 1
-			
-		elif(a == "não" or a == "nao" or a == "n" or a == "0"):
-			answers.append(0)
-			i += 1
-
-		else:
-			print("\nResposta Inválida\nResponda novamente\n")
-				
-
-	probabilities = df["probability"].to_list()
+def risk_analyser(answers, probabilities):
 	risk_sum = 0
 	for answer, probability in zip(answers, probabilities):
 
@@ -39,16 +13,54 @@ def main():
 			risk_sum += 1 	#medium risk client
 
 	#case risk sum of not common symptoms are greater than the half ammount of symptoms 
-	if(risk_sum > len(questions)/2):
+	if(risk_sum > len(probabilities)/2):
 		risk_sum = -1	#high riks client
 
+	return risk_sum
+
+def answer_standardize(a):
+	if(a == "sim" or a == "s" or a == "1"): return 1
+
+	elif(a == "não" or a == "nao" or a == "n" or a == "0"): return 0
+
+	else: return -1
+
+def main():
+	df = pd.read_csv("Data/symptoms.csv")
+	print(df["question"])
+
+	questions = df["question"].to_list()
+
+	answers = []
+	i = 0
+	while (i < len(questions)):
+		print(str(i +1) + '. '+ questions[i])
+
+		a = answer_standardize(input().lower())
+
+		if(a != -1):
+			answers.append(a)
+			i += 1
+	
+		else:
+			print("\nResposta Inválida\nResponda novamente\n")
+				
+
+	probabilities = df["probability"].to_list()
+	risk = risk_analyser(answers, probabilities)
+
 	#check what final message will be delivered	
-	if(risk_sum < 0):
+	if(risk < 0):
 		print("Chances altas de ter covid. Volte para casa e busque atendimento médico")
-	elif(risk_sum > 0):
+
+	elif(risk > 0):
 		print("Você possui alguns dos sintomas menos comuns da Covid-19.\nTalvez seja a hora de buscar atendimento médico.\nVocê poderá utilizar os serviços deste estabelecimento se seguir corretamente os protocolos de segurança:\n-Lavar Mãos\n-Usar Máscara")
+	
 	else:
 		print("Seja Bem-Vindo ao estabelecimento. Durante sua estadia use máscara e lembre-se de lavar as mãos com alcool em gel")
+
+
+	#send a message with the decision
 
 if __name__ == '__main__':
 	main()
